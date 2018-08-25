d5f9342d4a5ff95a00b472294cf7ee3a7b3587ce
rutterer <rutterer@gmail.com>
2018-08-24 17:00:20 -0700

    finished game logic (mostly)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+/* var alreadyGuessed = []; */
 
 var winText = document.getElementById("windiv");
 var lossText = document.getElementById("lossdiv");
@@ -11,28 +12,38 @@ var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 
 var compLetter = compChoices[Math.floor(Math.random() * compChoices.length)];
 
+
 document.onkeyup = function (event) {
 
     var playerGuess = event.key.toLowerCase();
 
-    console.log(playerGuess);
+  /*   alreadyGuessed.push(playerGuess); */
+
+
+if (compChoices.includes(playerGuess)) {
 
     if (playerGuess === compLetter) {
         wins++;
-    
+        compLetter = compChoices[Math.floor(Math.random() * compChoices.length)];
+        guessesLeft = 9;
+        guessesText.textContent = " ";
+
     } else if (playerGuess !== compLetter) {
         guessesLeft--;
     }
 
     if (guessesLeft === 0) {
         losses++;
+        guessesLeft = 9;
+        compLetter = compChoices[Math.floor(Math.random() * compChoices.length)];
+        guessesText.textContent = " ";
     }
 
-    winText.textContent = "" + wins;
-    lossText.textContent = "" + losses;
-    guessLeftText.textContent = "" + guessesLeft;
-    guessesText.textContent = "" + playerGuess;
+    winText.textContent =  wins;
+    lossText.textContent =  losses;
+    guessLeftText.textContent =  guessesLeft;
+    guessesText.textContent += " " + playerGuess;
  
 }
 
-
+}
